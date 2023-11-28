980388bc093942a6e57f28d05eebd34c0bf28b4a
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-28 17:37:46 -0500

    Add create user DTO

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
-  create(CreateUserDto: CreateUserDto) {
-    return 'This action adds a new auth';
-  }
+	
+	constructor( @InjectModel( User.name ) private userModel: Model<User> ) {}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+	async create(CreateUserDto: CreateUserDto): Promise<User> {
+		// console.log(CreateUserDto); 
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+		try {
+			const newUser = new this.userModel( CreateUserDto );
+			return await newUser.save();
+		} catch (error) {
+			if ( error.code === 11000 ) {
+				throw new BadRequestException(`${ CreateUserDto.email } already exists!`)
+			}
+			throw new InternalServerErrorException('Something terrible happen!')
+		}
+	}
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+	findAll() {
+		return `This action returns all auth`;
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+	findOne(id: number) {
+		return `This action returns a #${id} auth`;
+	}
+
+	update(id: number, updateAuthDto: UpdateAuthDto) {
+		return `This action updates a #${id} auth`;
+	}
+
+	remove(id: number) {
+		return `This action removes a #${id} auth`;
+	}
 }
