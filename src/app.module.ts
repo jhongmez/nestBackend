768749a0a83ff159725466a5b34f6f6dc1fa1ac1
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-28 07:04:34 -0500

    Add models and schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     AuthModule,
-    MongooseModule.forRoot('mongodb://localhost:27017')
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+}
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+// * Propiedades de la BD
+export class User {
+
+   @Prop({ unique: true, required: true})
+	email: string;
+
+   @Prop({ required: true})
+   name: string;
+
+   @Prop({ minlength: 6, required: true })
+   password: string;
+
+   @Prop({ default: true })
+   isActive: boolean;
+
+   @Prop({ type: [String], default: ['user']})
+   roles: string[];
+
+}
+
+export const UserSchema = SchemaFactory.createForClass( User );
