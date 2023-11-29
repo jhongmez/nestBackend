36cf1fa7f3824fb9c88464e5e0bd313f41094ef7
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-29 08:26:45 -0500

    Generate new JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request }
 import { CreateUserDto, UpdateAuthDto, LoginDto, RegisterDto } from './dto/index';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './guards/auth.guard';
+import { User } from './entities/user.entity';
+import { LoginResponse } from './interfaces/login-response';
 
 @Controller('auth')
 export class AuthController {
@@ -29,19 +31,30 @@ export class AuthController {
 		// return user;
 		return this.authService.findAll();
 	}
+	
+	@UseGuards( AuthGuard )
+	@Get('/check-token')
+	checkToken( @Request() req: Request ): LoginResponse {
+		const user = req['user'] as User;
 
-	@Get(':id')
-	findOne(@Param('id') id: string) {
-		return this.authService.findOne(+id);
+		return {
+			user,
+			token: this.authService.getJwtToken({ id: user._id })
+		}
 	}
 
-	@Patch(':id')
-	update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-		return this.authService.update(+id, updateAuthDto);
-	}
+	// @Get(':id')
+	// findOne(@Param('id') id: string) {
+	// 	return this.authService.findOne(+id);
+	// }
 
-	@Delete(':id')
-	remove(@Param('id') id: string) {
-		return this.authService.remove(+id);
-	}
+	// @Patch(':id')
+	// update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+	// 	return this.authService.update(+id, updateAuthDto);
+	// }
+
+	// @Delete(':id')
+	// remove(@Param('id') id: string) {
+	// 	return this.authService.remove(+id);
+	// }
 }
